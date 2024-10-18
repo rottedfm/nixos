0198f896e5fa3dde9c6f6c2d9fe25788703f0ab3
rottedfm <rottedfm@proton.me>
2024-10-17 22:01:41 -0400

    Thu Oct 17 10:01:41 PM EDT 2024

--- homeManager/modules/zellij.nix
+++ homeManager/modules/zellij.nix
@@ -2,8 +2,16 @@
 
 {
   programs.zellij = {
-    enable = true;
     enableZshIntegration = true;
     package = pkgs.unstable.zellij;
+    settings = {
+      ui = {
+        pane_frames = {
+          rounded_corners = false;
+          hide_session_name = true;
+        };
+      };
+      mouse_mode = false;
+    };
   };
 }
