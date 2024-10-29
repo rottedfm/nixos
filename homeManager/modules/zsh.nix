ee1d70a7ad5cc70888e23f7df57ddb56a98b7778
rottedfm <rottedfm@proton.me>
2024-10-29 17:40:34 -0400

    Tue Oct 29 05:40:34 PM EDT 2024

--- homeManager/modules/zsh.nix
+++ homeManager/modules/zsh.nix
@@ -12,7 +12,7 @@
       ttyper = "ttyper -l rust -w 20";
       rust = "nix develop ~/.dotfiles/rust/";
       python = "nix develop ~/.dotfiles/python";
-      hyprland = "hyprland | clear";
+      hyprland = "hyprland & clear";
     };
 
     initExtra = ''
