878bbc0225de5ea9a575e38ad94292a0c6a177e4
rottedfm <rottedfm@proton.me>
2024-11-04 08:52:24 -0500

    Mon Nov  4 08:52:24 AM EST 2024

--- nixos/modules/mullvad.nix
+++ nixos/modules/mullvad.nix
@@ -2,4 +2,15 @@
 
 {
   services.mullvad-vpn.enable = true;
+
+  systemd.services.mullvad-vpn = {
+    description = "Mullvad VPN Service";
+    wantedBy = [ "multi-user.target"];
+    after = [ "network.target" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.mullvad-vpn}/bin/mullvad connect";
+      Restart = "on-failure";
+    };
+  };
 }
