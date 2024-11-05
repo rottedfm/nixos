9d66a1bfbe1984c62baf4bff5d4e9e63feed8a89
rottedfm <rottedfm@proton.me>
2024-11-05 16:07:17 -0500

    Tue Nov  5 04:07:17 PM EST 2024

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -6,4 +6,22 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    steam
+    gamescope
+    mangohud
+  ];
+
+    # Environment variables to improve Wayland support in Steam
+  environment.variables = {
+    "STEAM_RUNTIME" = "0";           # Disables Steam’s bundled runtime, using system libraries instead
+    "SDL_VIDEODRIVER" = "wayland";   # Set SDL to use Wayland
+    "MANGOHUD" = "1";                # Enables MangoHud for system metrics display
+  };
+
+  # Optional: Add an alias for running Steam with Gamescope
+  environment.shellAliases = {
+    "steam-wayland" = "gamescope -f -e -- steam";
+  };
 }
