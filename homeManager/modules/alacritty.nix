08df2a04337e6f0befbcddee145dbf70879cf3f2
rottedfm <rottedfm@proton.me>
2024-11-14 13:08:31 -0500

    Thu Nov 14 01:08:31 PM EST 2024

--- homeManager/modules/alacritty.nix
+++ homeManager/modules/alacritty.nix
@@ -4,5 +4,13 @@
   programs.alacritty = {
     enable = true;
     package = pkgs.unstable.alacritty;
+    settings = {
+      font = {
+        family = "Fira Code";
+        features = {
+          liga = true;
+        };
+      };
+    };
   };
 }
